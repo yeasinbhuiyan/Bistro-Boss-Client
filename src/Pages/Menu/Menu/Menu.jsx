cd8fa038306908d1e79635b5902a0cb5d27337e7
yeasinbhuiyan <yeasin382255656@gmail.com>
2023-05-24 15:07:26 +0600

    menu cover paralx complete

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
         <div>
             <Helmet>
-                <title>Bistro Boss| Menu</title>
+
+                <title>Bistro Boss | Menu</title>
             </Helmet>
+            <Cover
+                img={menuImg}
+                title={"Our Menu"}>
+            </Cover>
+            <PopularMenu></PopularMenu>
+
+            <Cover
+                img={menuImg}
+                title={"Our Menu"}>
+            </Cover>
+            <PopularMenu></PopularMenu>
+
+            
+            <Cover
+                img={menuImg}
+                title={"Our Menu"}>
+            </Cover>
+            <PopularMenu></PopularMenu>
+        
             <h1> men men menu</h1>
         </div>
     );
